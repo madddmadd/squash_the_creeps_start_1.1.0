820fbd9fe6e5402861d186f89ef83cabc18df43b
madddmadd <marcusdogan161@gmail.com>
2025-01-16 13:45:45 -0600

    bsdfsdfd
    sdfsd

--- mob.gd
+++ mob.gd
@@ -1,30 +1,32 @@
 extends CharacterBody3D
 
-
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
-
-
-func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
-
+# Minimum speed of the mob in meters per second.
+@export var min_speed = 10
+# Maximum speed of the mob in meters per second.
+@export var max_speed = 18
+signal squashed
+func _physics_process(_delta):
 	move_and_slide()
+
+# This function will be called from the Main scene.
+func initialize(start_position, player_position):
+	# We position the mob by placing it at start_position
+	# and rotate it towards player_position, so it looks at the player.
+	look_at_from_position(start_position, player_position, Vector3.UP)
+	# Rotate this mob randomly within range of -45 and +45 degrees,
+	# so that it doesn't move directly towards the player.
+	rotate_y(randf_range(-PI / 4, PI / 4))
+
+	# We calculate a random speed (integer)
+	var random_speed = randi_range(min_speed, max_speed)
+	# We calculate a forward velocity that represents the speed.
+	velocity = Vector3.FORWARD * random_speed
+	# We then rotate the velocity vector based on the mob's Y rotation
+	# in order to move in the direction the mob is looking.
+	velocity = velocity.rotated(Vector3.UP, rotation.y)
+
 func _on_visible_on_screen_notifier_3d_screen_exited():
+	queue_free()
+func squash():
+	squashed.emit()
 	queue_free()
--- node_3d.gd
+++ node_3d.gd
@@ -1,10 +1,14 @@
 extends CharacterBody3D
 
+@export var bounce_impulse = 16
 @export var speed=15
 @export var fall_accel=75
+@export var jump_impulse = 200
+
 var target_velocity = Vector3.ZERO
 func _physics_process(delta):
-	# We create a local variable to store the input direction.
+	if is_on_floor() and Input.is_action_just_pressed("jump"):
+		target_velocity.y = jump_impulse
 	var direction = Vector3.ZERO
 
 	# We check for each move input and update the direction accordingly.
@@ -33,3 +37,21 @@ func _physics_process(delta):
 	# Moving the Character
 	velocity = target_velocity
 	move_and_slide()
+	for index in range(get_slide_collision_count()):
+		# We get one of the collisions with the player
+		var collision = get_slide_collision(index)
+
+		# If the collision is with ground
+		if collision.get_collider() == null:
+			continue
+
+		# If the collider is with a mob
+		if collision.get_collider().is_in_group("mob"):
+			var mob = collision.get_collider()
+			# we check that we are hitting it from above.
+			if Vector3.UP.dot(collision.get_normal()) > 0.1:
+				# If so, we squash it and bounce.
+				mob.squash()
+				target_velocity.y = bounce_impulse
+				# Prevent further duplicate calls.
+				break
